8ec16ab452b22942e54a0a312c9549a540bd5635
chanu <yuyutyanu@yahoo.co.jp>
2017-10-05 20:44:38 +0900

    fix structure

--- gulpfile.js
+++ gulpfile.js
@@ -7,53 +7,54 @@ var pug = require("gulp-pug");
 var path = require("path")
 
 gulp.task('default', ['scss', 'browser-sync', 'pug', 'watch']); //コマンドプロンプトからgulpと実行された場合に行うタスクの一覧
+gulp.task('build',['scss','pug'])
 
 //scssとpugの監視をして変換処理させる
 gulp.task('watch', () => {
-    gulp.watch([path.resolve('./public/src/scss/**.scss')], () => {
-        gulp.start(['scss']);
-    });
-    gulp.watch([path.resolve('./public/src/pug/**.pug')], () => {
-        gulp.start(['pug']);
-    });
+  gulp.watch([path.resolve('./src/scss/**.scss')], () => {
+    gulp.start(['scss']);
+  });
+  gulp.watch([path.resolve('./src/pug/**.pug')], () => {
+    gulp.start(['pug']);
+  });
 });
 
 //scssをcssに変換
 gulp.task("scss", () => {
-    gulp.src(path.resolve('./public/src/scss/**.scss'))
-        .pipe(plumber({
-            errorHandler: notify.onError("Error: <%= error.message %>")
-        }))
-        .pipe(scss())
-        .pipe(gulp.dest("./dist/css"))
+  gulp.src(path.resolve('./src/scss/**.scss'))
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>")
+    }))
+    .pipe(scss())
+    .pipe(gulp.dest("./dist/views/css"))
 });
 
 //pugをhtmlに変換
 gulp.task("pug", () => {
-    gulp.src(path.resolve('./public/src/pug/**.pug'))
-        .pipe(plumber({
-            errorHandler: notify.onError("Error: <%= error.message %>")
-        }))
-        .pipe(pug())
-        .pipe(gulp.dest("./dist/html"))
+  gulp.src(path.resolve('./src/pug/**.pug'))
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>")
+    }))
+    .pipe(pug())
+    .pipe(gulp.dest("./dist/views"))
 });
 
 
 //ブラウザ表示
 gulp.task('browser-sync', () => {
-    browserSync({
-        server: {
-            baseDir: "./dist/html"   //ブラウザに表示するディレクトリ
-        }
-    });
-    //ファイルの監視
-    //以下のファイルが変わったらリロードする
-    gulp.watch(path.resolve("./dist/js/*.js"),     ['reload']);
-    gulp.watch(path.resolve("./dist/html/*.html"),     ['reload']);
-    gulp.watch(path.resolve("./dist/css/*.css"),     ['reload']);
+  browserSync({
+    server: {
+      baseDir: "./dist/views"   //ブラウザに表示するディレクトリ
+    }
+  });
+  //ファイルの監視
+  //以下のファイルが変わったらリロードする
+  gulp.watch(path.resolve("./dist/views/js/*.js"),       ['reload']);
+  gulp.watch(path.resolve("./dist/views/*.html"),        ['reload']);
+  gulp.watch(path.resolve("./dist/views/css/*.css"),     ['reload']);
 });
 
 //ブラウザリロード処理
 gulp.task('reload', () => {
-    browserSync.reload();
+  browserSync.reload();
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
 const path = require("path")
+const webpack = require('webpack')
 
 module.exports = {
-    entry: {
-        bundle: [
-            path.resolve('./public/src/js/index.js'),
-            path.resolve('./public/src/js/sample.js')
-        ]
-    },
-    output: {
-        path: path.resolve("./dist"),
-        filename: '[name].js',
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: "babel-loader"
-            }
-        ],
-    }
+  entry: {
+    bundle: [
+      path.resolve('./public/src/js/index.js'),
+      path.resolve('./public/src/js/sample.js')
+    ]
+  },
+  output: {
+    path: path.resolve("./dist/views/js"),
+    filename: '[name].js',
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }
+    ],
+  }
 }
